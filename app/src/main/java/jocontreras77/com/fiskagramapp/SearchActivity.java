0f17e9303fea293d9c2687e411686249d6e7c71c
jocontreras <jen.almarasy@gmail.com>
2017-08-29 08:40:06 +0200

    add navigation menu using switch statement

---
+++ app/src/main/java/jocontreras77/com/fiskagramapp/SearchActivity.java
@@ -0,0 +1,36 @@
+package jocontreras77.com.fiskagramapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import jocontreras77.com.fiskagramapp.Utils.BottomNavigationViewHelper;
+
+/**
+ * Created by Jennifer on 8/29/17.
+ */
+
+public class SearchActivity extends AppCompatActivity {
+    private static final String TAG = "SearchActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Log.d(TAG,"onCreate: Started");
+
+        setupBottomNavigationView();
+    }
+
+    /**
+     * BottomNavigationView Setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+    }
+}
--- app/src/main/java/jocontreras77/com/fiskagramapp/Utils/BottomNavigationViewHelper.java
+++ app/src/main/java/jocontreras77/com/fiskagramapp/Utils/BottomNavigationViewHelper.java
@@ -1,7 +1,20 @@
 package jocontreras77.com.fiskagramapp.Utils;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.util.Log;
+import android.view.MenuItem;
+
+import jocontreras77.com.fiskagramapp.HomeActivity;
+import jocontreras77.com.fiskagramapp.LikesActivity;
+import jocontreras77.com.fiskagramapp.ProfileActivity;
+import jocontreras77.com.fiskagramapp.R;
+import jocontreras77.com.fiskagramapp.SearchActivity;
+import jocontreras77.com.fiskagramapp.ShareActivity;
 
 /**
  * Created by Jennifer on 8/28/17.
@@ -15,6 +28,37 @@ public class BottomNavigationViewHelper {
         bottomNavigationViewEx.enableItemShiftingMode(false);
         bottomNavigationViewEx.enableShiftingMode(false);
         bottomNavigationViewEx.setTextVisibility(false);
+    }
+
+    public  static void enableTabNavigation(final Context context, BottomNavigationViewEx view){
+        view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_house:
+                        Intent intent1 = new Intent(context, HomeActivity.class); //ACTIVITY_NUM = 0
+                        context.startActivity(intent1);
+                        break;
+                    case R.id.ic_search:
+                        Intent intent2 = new Intent(context, SearchActivity.class); //ACTIVITY_NUM = 1
+                        context.startActivity(intent2);
+                        break;
+                    case R.id.ic_circle:
+                        Intent intent3 = new Intent(context, ShareActivity.class); //ACTIVITY_NUM = 2
+                        context.startActivity(intent3);
+                        break;
+                    case R.id.ic_alert:
+                        Intent intent4 = new Intent(context, LikesActivity.class); //ACTIVITY_NUM = 3
+                        context.startActivity((intent4));
+                        break;
+                    case R.id.ic_android:
+                        Intent intent5 = new Intent(context, ProfileActivity.class); //ACTIVITY_NUM = 4
+                        context.startActivity(intent5);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
 }
